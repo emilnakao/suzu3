4fbc9064ee62daee8601a8eacc54025546f94fa1
Emil Yoshigae Nakao <emilnakao@gmail.com>
2016-11-26 17:25:38 -0200

    Configurando chrome no travis

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,12 @@
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.dev');
 
+/**
+ * tip for running chrome on travis found on
+ * https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
+ *
+ * @param config
+ */
 module.exports = function(config){
     config.set({
         browsers: ['Chrome'],
@@ -20,6 +26,16 @@ module.exports = function(config){
         webpack: {
             module: webpackConfig.module,
             resolve: webpackConfig.resolve
-        }
-    })
+        },
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+    });
+
+    if(process.env.TRAVIS){
+        config.browsers = ['Chrome_travis_ci'];
+    }
 }
