ec6a578798cc3445264a5e3667dfe1bd74c47242
emilnakao <emilnakao@gmail.com>
2020-07-14 06:59:43 -0300

    Delete HomePage.test.jsx

--- src/pages/HomePage.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from "./HomePage";
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<HomePage/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
